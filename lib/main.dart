4b82284a1cc6f8163f2cb17a9f47ef4217982b73
luckydarwai <luckydarvai545@gmail.com>
2023-03-20 16:06:26 +0530

    Row and colom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,133 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(myApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class myApp extends StatefulWidget {
+  const myApp({super.key});
+
+  @override
+  State<myApp> createState() => homePage();
+}
+
+class homePage extends State<myApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter Images !"),
-      ),
-      body: Center(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Row And Coloumn',
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
           child: Container(
-              height: 100,
-              width: 100,
-              child: Image.asset("assets/images/rose2.jpg"))),
+            // height: 350,
+            // width: 350,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        "A",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text("B", style: TextStyle(fontSize: 20)),
+                      Text("C", style: TextStyle(fontSize: 20)),
+                      Text("D", style: TextStyle(fontSize: 20)),
+                      Text("E", style: TextStyle(fontSize: 20)),
+                    ]),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        "E",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        "F",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        "G",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        "H",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        "I",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ]),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          "assets/images/apple.jpg",
+                          height: 200,
+                          width: 200,
+                        ),
+                        Image.asset(
+                          "assets/images/apple.jpg",
+                          height: 200,
+                          width: 200,
+                        ),
+                        Image.asset(
+                          "assets/images/apple.jpg",
+                          height: 200,
+                          width: 200,
+                        ),
+                        Image.asset(
+                          "assets/images/apple.jpg",
+                          height: 200,
+                          width: 200,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          "assets/images/mango.jpg",
+                          height: 200,
+                          width: 200,
+                        ),
+                        Image.asset(
+                          "assets/images/mango.jpg",
+                          height: 200,
+                          width: 200,
+                        ),
+                        Image.asset(
+                          "assets/images/mango.jpg",
+                          height: 200,
+                          width: 200,
+                        ),
+                        Image.asset(
+                          "assets/images/mango.jpg",
+                          height: 200,
+                          width: 200,
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
