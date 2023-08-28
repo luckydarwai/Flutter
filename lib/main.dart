c2818d99524821757ce3a6684ebdbac20d1e3ef1
luckydarwai <luckydarvai545@gmail.com>
2023-08-28 12:40:52 +0530

    flutter application development

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyHomePage());
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  callBack() {
-    print("clicked>>>>>");
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Callback Function"),
+          title: Text("Splitting into widgets"),
         ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: callBack,
-            child: Text(
-              "Click Me",
+        body: Container(
+          child: Column(children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.amber,
+                child: ListView.builder(
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: 100,
+                      child: CircleAvatar(backgroundColor: Colors.red),
+                    ),
+                  ),
+                  itemCount: 10,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
             ),
-          ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.orange,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.green,
+              ),
+            ),
+          ]),
         ),
       ),
     );
